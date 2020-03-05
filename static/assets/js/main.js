5d5c3209aadf405c1e513f39b758aab1d2c428a7
Ben White <benny.boy1695@gmail.com>
2020-03-05 07:04:13 +0000

    Working plaercount !

---
+++ static/assets/js/main.js
@@ -0,0 +1,23 @@
+const playersEle = document.getElementById("players");
+let cachedCount;
+
+function playerCount(element) {
+    var playerCountURL = "https://shadownode.ca/servers/api/getPlayerCount.json";
+    fetch(playerCountURL, {
+        method: 'get'
+    }).then(async function (response) {
+        const json = await response.json();
+        cachedCount = "There are " + json.players + " players online!";
+        element.innerText = cachedCount;
+    }).catch(function (err) {
+        console.log("Error: " + err)
+    });
+}
+
+playersEle.onloadstart = (playerCount(playersEle));
+playersEle.onmouseenter = function () {
+    playersEle.innerText = "Ip: play.shadownode.ca";
+};
+playersEle.onmouseleave = function () {
+    playersEle.innerText = cachedCount;
+};
