aac151a86e1be0908b88f58845c53f737e6c68ef
Alexandrea <Alexandrea@users.noreply.github.com>
2020-03-11 13:19:52 +0000

    Merge pull request #13 from Alexandrea/site
    More fixes and changes e13936f694d93adbe8f97dcb4fb41f493faae0cb

--- assets/js/breadCrumb.js
+++ assets/js/breadCrumb.js
@@ -1,7 +1,13 @@
 const breadCrumb = document.getElementById("toc-menu");
-
-function updateBreadCrumb() {
-    breadCrumb.innerHTML = "<i class=\"fas fa-list-alt\"></i> Table of Contents";
+breadCrumb.hidden = true;
+const tableOfContents = document.getElementById("TableOfContents");
+if (!String.prototype.trim) {
+    String.prototype.trim = function() { return this.replace(/^\s+|\s+$/, ''); };
 }
-
+function updateBreadCrumb() {
+    if (tableOfContents.innerHTML.trim() !== "") {
+        breadCrumb.innerHTML = "<i class=\"fas fa-list-alt\"></i> Table of Contents";
+        breadCrumb.hidden = false;
+    }
+}
 breadCrumb.onloadstart = (updateBreadCrumb());
