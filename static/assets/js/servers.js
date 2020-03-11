cd44078fd1a63919e28044b791d0f25e2aa191a7
Ben White <benny.boy1695@gmail.com>
2020-03-11 23:51:02 +0000

    Millies fixes and start of extended bit

--- static/assets/js/servers.js
+++ static/assets/js/servers.js
@@ -10,51 +10,27 @@ function getAllServers() {
         for (var key of Object.keys(servers)) {
             const section = servers[key];
             console.log(section);
-            addServer(section.name, (section.status ? "online" : "offline"), section.pack_name, section.pack_version, "?/" + section.maxplayers, section.uptime);
+            var template = document.getElementById('server-template');
+            addServer(template.content.cloneNode(true), section.name, (section.status ? "online" : "offline"), section.pack_name, section.pack_version, "?/" + section.maxplayers, section.uptime, section.address);
         }
     }).catch(function (err) {
         console.log("Error: " + err)
     });
 }
 
-function addServer(name, online, pack, packVersion, playerCount, uptime) {
-    var col = document.createElement('div');
-    col.setAttribute('class', 'col')
-    divServerTable.appendChild(col);
+function addServer(element, name, online, pack, packVersion, playerCount, uptime, serverIp) {
+    element.getElementById('status').classList.add(online);
+    element.getElementById('server-name').innerText = name;
+    element.getElementById('pack').innerText = pack;
+    element.getElementById('pack-version').innerText = packVersion;
+    element.getElementById('players').innerText = playerCount;
+    element.getElementById('uptime').innerText = uptime;
+    element.getElementById('ip').innerText = serverIp;
+    divServerTable.appendChild(element);
+}
 
-    var table = document.createElement('div');
-    table.setAttribute('class', 'table');
-    col.appendChild(table);
-
-    //Status Div
-    var status = document.createElement('div');
-    status.setAttribute('class', 'status ' + online);
-    table.appendChild(status);
-    var h2Name = document.createElement('h2');
-    h2Name.innerText = name;
-    status.appendChild(h2Name);
-
-    //Info Div
-    var dl = document.createElement('dl');
-    dl.setAttribute('class', 'info');
-    table.appendChild(dl);
-    var packDT = document.createElement('dt');
-    packDT.innerText = "Pack:";
-    var packDD = document.createElement('dd');
-    packDD.innerText = pack;
-    var packVersionDT = document.createElement('dt');
-    packVersionDT.innerText = "Pack Version:";
-    var packVersionDD = document.createElement('dd');
-    packVersionDD.innerText = packVersion;
-    var playersDT = document.createElement('dt');
-    playersDT.innerText = "Players:";
-    var playersDD = document.createElement('dd');
-    playersDD.innerText = playerCount
-    var uptimeDT = document.createElement('dt');
-    uptimeDT.innerText = "Uptime:";
-    var uptimeDD = document.createElement('dd');
-    uptimeDD.innerText = uptime;
-    dl.append(packDT, packDD, packVersionDT, packVersionDD, playersDT, playersDD, uptimeDT, uptimeDD);
+function extendInfo(element) {
+    element.parentElement.getElementsByClassName('extended-info')[0].classList.toggle('active');
 }
 
 getAllServers();
