e2614197d0b8a5b455bfd5dac6275b37bd9e7b14
Alexandrea <Alexandrea@users.noreply.github.com>
2020-03-12 12:32:28 +0000

    Merge pull request #15 from Bennyboy1695/site
    Project servers page 04b7cef91888481dce50378b0703c59b9144344e

--- assets/js/servers.js
+++ assets/js/servers.js
@@ -1,37 +1,51 @@
-var ctx = document.getElementById('tpschart').getContext('2d');
-var tps = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+const divServerTable = document.getElementById("server-table");
+
+function getAllServers() {
+    var statsUrl = "https://shadownode.ca/servers/api/getStatsData.json";
+    fetch(statsUrl, {
+        method: 'get'
+    }).then(async function (response) {
+        const json = await response.json();
+        const servers = json.servers;
+        for (var key of Object.keys(servers)) {
+            const section = servers[key];
+            console.log(section);
+            var template = document.getElementById('server-template');
+            addServer(template.content.cloneNode(true), key, section.name, (section.status ? "online" : "offline"), section.pack_name, section.pack_version, "?/" + section.maxplayers, section.uptime, section.address);
         }
-    }
-});
+        loaded();
+    }).catch(function (err) {
+        console.log("Error: " + err)
+    });
+}
+
+function addServer(element, id, name, online, pack, packVersion, playerCount, uptime, serverIp) {
+    element.getElementById('status').classList.add(online);
+    element.getElementById('status').id = id + "_status";
+    element.getElementById('server-name').innerText = name;
+    element.getElementById('server-name').id = id + "_server-name";
+    element.getElementById('pack').innerText = pack;
+    element.getElementById('pack').id = id + "_pack";
+    element.getElementById('pack-version').innerText = packVersion;
+    element.getElementById('pack-version').id = id + "_pack-version";
+    element.getElementById('players').innerText = playerCount;
+    element.getElementById('players').id = id + "_players";
+    element.getElementById('uptime').innerText = uptime;
+    element.getElementById('uptime').id = id + "_uptime";
+    element.getElementById('ip').innerText = serverIp;
+    element.getElementById('ip').id = id + "_ip";
+    element.getElementById('player-chart').id = id + "_player-chart";
+    element.getElementById('tps-chart').id = id + "_tps-chart";
+    divServerTable.appendChild(element);
+}
+
+function extendInfo(element) {
+    element.parentElement.getElementsByClassName('extended-info')[0].classList.toggle('active');
+}
+
+function loaded() {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('serversWrapper').style.display = 'flex';
+}
+
+getAllServers();
