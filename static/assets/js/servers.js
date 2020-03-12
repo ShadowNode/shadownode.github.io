3346f5dd2981a95638a5eea383bb0ea1c65c57e1
Ben White <benny.boy1695@gmail.com>
2020-03-12 12:22:24 +0000

    Create loading screen & each table is assigned serverId

--- static/assets/js/servers.js
+++ static/assets/js/servers.js
@@ -11,26 +11,41 @@ function getAllServers() {
             const section = servers[key];
             console.log(section);
             var template = document.getElementById('server-template');
-            addServer(template.content.cloneNode(true), section.name, (section.status ? "online" : "offline"), section.pack_name, section.pack_version, "?/" + section.maxplayers, section.uptime, section.address);
+            addServer(template.content.cloneNode(true), key, section.name, (section.status ? "online" : "offline"), section.pack_name, section.pack_version, "?/" + section.maxplayers, section.uptime, section.address);
         }
+        loaded();
     }).catch(function (err) {
         console.log("Error: " + err)
     });
 }
 
-function addServer(element, name, online, pack, packVersion, playerCount, uptime, serverIp) {
+function addServer(element, id, name, online, pack, packVersion, playerCount, uptime, serverIp) {
     element.getElementById('status').classList.add(online);
+    element.getElementById('status').id = id + "_status";
     element.getElementById('server-name').innerText = name;
+    element.getElementById('server-name').id = id + "_server-name";
     element.getElementById('pack').innerText = pack;
+    element.getElementById('pack').id = id + "_pack";
     element.getElementById('pack-version').innerText = packVersion;
+    element.getElementById('pack-version').id = id + "_pack-version";
     element.getElementById('players').innerText = playerCount;
+    element.getElementById('players').id = id + "_players";
     element.getElementById('uptime').innerText = uptime;
+    element.getElementById('uptime').id = id + "_uptime";
     element.getElementById('ip').innerText = serverIp;
+    element.getElementById('ip').id = id + "_ip";
+    element.getElementById('player-chart').id = id + "_player-chart";
+    element.getElementById('tps-chart').id = id + "_tps-chart";
     divServerTable.appendChild(element);
 }
 
 function extendInfo(element) {
     element.parentElement.getElementsByClassName('extended-info')[0].classList.toggle('active');
+}
+
+function loaded() {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('serversWrapper').style.display = 'flex';
 }
 
 getAllServers();
