34da0302bbb1383de159187c94cf94b215fd1490
Matt M <MatthewM@users.noreply.github.com>
2022-01-07 08:20:58 -0500

    Updated layout and made it more static

--- static/assets/js/voters.js
+++ static/assets/js/voters.js
@@ -2,48 +2,29 @@ const headAPI = "https://mc-heads.net/avatar/";
 const voterTable = document.getElementById("voter-table");
 
 function getCurrentTop3() {
-    /*
-        Example Code 
-        This is example JSON output till we have an actual format
-    */ 
-    // const top3 = [
-    //     {
-    //         uuid: "90349245-dd9a-464c-baf7-f72fa414b5a1",
-    //         pname: "LethalMrM",
-    //         votes: 10    
-    //     },
-    //     {
-    //         uuid: "df6e1e9b-d23d-4283-a941-87916bc3ea2e",
-    //         pname: "ShadowsKitten",
-    //         votes: 9
-    //     },
-    //     {
-    //         uuid: "a97174eb-9e37-4035-b751-f7601488528d",
-    //         pname: "Bennyboy1695",
-    //         votes: 100
-    //     }
     var currentTop3 = "https://api.shadownode.ca/voters/?method=getTopVoters&when=thismonth"
 
     fetch(currentTop3, {
         method: 'get'
     }).then(async function (response) {
+
         const json = await response.json();
         const top3 = json.response;
         
-       voterTable.innerHTML += '<div class="voter-heading"><h3>Current Top 3 Voters</h3></div>';
+       var cTable = document.getElementById('current-top');
 
         for (var key of Object.keys(top3)) {
             const section = top3[key];
+
             var template = document.getElementById('voter-template');
     
             var uuid = section.uuid;
             var pname = section.username;
             var votes = section.votes_this_month;
-    
-            addVoter(template.content.cloneNode(true), key, uuid, pname, votes);
+
+            addVoter(template.content.cloneNode(true), key, uuid, pname, votes, cTable);
         }
     });
-    getLastTop3();
 }
 function getLastTop3() {
     var apiURL = "https://api.shadownode.ca/voters/?method=getTopVoters&when=lastmonth"
@@ -51,24 +32,25 @@ function getLastTop3() {
     fetch(apiURL, {
         method: 'get'
     }).then(async function (response) {
+
         const json = await response.json();
         const top3 = json.response;
         
-       voterTable.innerHTML += '<div class="voter-heading"><h3>Last Month\'s Top 3 Voters</h3></div>';
-
+    //    voterTable.innerHTML += '<div class="voter-heading"><h3>Last Month\'s Top 3 Voters</h3></div>';
+        var lTable = document.getElementById('last-top');
         for (var key of Object.keys(top3)) {
             const section = top3[key];
+
             var template = document.getElementById('voter-template');
     
             var uuid = section.uuid;
             var pname = section.username;
             var votes = section.votes_last_month;
     
-            addVoter(template.content.cloneNode(true), key, uuid, pname, votes);
+            addVoter(template.content.cloneNode(true), key, uuid, pname, votes, lTable);
         }
     });
 
-    getTotalTokens();
 }
 
 
@@ -78,21 +60,23 @@ function getTotalTokens() {
     fetch(apiURL, {
         method: 'get'
     }).then(async function (response) {
+
         const json = await response.json();
         const top3 = json.response;
         
-       voterTable.innerHTML += '<div class="voter-heading"><h3>Top 3 Tokens</h3></div>';
+        var cTable = document.getElementById('token-top');
 
         for (var key of Object.keys(top3)) {
+         
             const section = top3[key];
+         
             var template = document.getElementById('voter-template');
     
             var uuid = section.uuid;
             var pname = section.username;
             var tokens = section.tokens;
     
-            // addVoter(template.content.cloneNode(true), key, uuid, pname, votes);
-            
+   
             element = template.content.cloneNode(true);
 
             var voterHead = element.getElementById('voter-head');
@@ -104,13 +88,13 @@ function getTotalTokens() {
             voterData.innerHTML = pname + "<br/>Has " + tokens + " tokens";
             voterData.id = pname + "_voter-data";
 
-            voterTable.appendChild(element);
+            cTable.appendChild(element);
         }
     });
 }
 
 
-function addVoter(element, voterPos, uuid, pname, voteCount) {
+function addVoter(element, voterPos, uuid, pname, voteCount, voterDiv) {
 
     var voterHead = element.getElementById('voter-head');
     var voterData = element.getElementById('voter-data');    
@@ -121,8 +105,10 @@ function addVoter(element, voterPos, uuid, pname, voteCount) {
     voterData.innerHTML = pname + "<br/>Voted " + voteCount + " times!";
     voterData.id = pname + "_voter-data";
 
-    voterTable.appendChild(element);
+    voterDiv.appendChild(element);
 }
 
 
 getCurrentTop3();
+getLastTop3();
+getTotalTokens();
